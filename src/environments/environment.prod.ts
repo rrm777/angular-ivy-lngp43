46f001c689320a410b5f3fe82a79db5dcc0e3523
rrm777 <rrm777@gmail.com>
2022-04-05 22:31:25 -0500

    2 files modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
-cognito: {
-  userPoolId: 'us-east-1_tUeWzI2Hq',
-  userPoolWebClientId: '46rnijqq57vcd0di06vrbmqhrf',
-},
+export const environment = {
+  production: true,
+  cognito: {
+    userPoolId: 'us-east-1_tUeWzI2Hq',
+    userPoolWebClientId: '46rnijqq57vcd0di06vrbmqhrf',
+  },
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
-cognito: {
-  userPoolId: 'us-east-2_pptCj2gqV',
-  userPoolWebClientId: '1452opnjll0ldmocs201b1oimu',
-},
+
+export const environment = {
+  production: false,
+  cognito: {
+    userPoolId: 'us-east-2_pptCj2gqV',
+    userPoolWebClientId: '1452opnjll0ldmocs201b1oimu',
+  },
+};
